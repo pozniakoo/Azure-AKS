7148612cfe7a85b719b9a0dd4bab3db7219f3fbd
Szymon <damsepro@gmail.com>
2025-11-11 18:15:43 +0100

    kubelet.identity -> kubelet_identity

--- Terraform/K8s/aks.tf
+++ Terraform/K8s/aks.tf
@@ -38,6 +38,6 @@ data "azurerm_client_config" "example" {
 resource "azurerm_role_assignment" "aks-acr-assignment" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks-cluster.kubelet.identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks-cluster.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
 }
