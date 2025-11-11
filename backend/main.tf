15038c47808d1aa646226b4df31cf0a1ed830c9d
Szymon <damsepro@gmail.com>
2025-11-11 17:38:41 +0100

    correction

--- backend/main.tf
+++ backend/main.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm"{
     resource_group_name = "tfstate-rg"
     storage_account_name = "szymon-tfstate-storage"
-    containercontainer_name = "tfstate"
+    container_name = "tfstate"
     key = "backend-state.tfstate"
   }
 }
